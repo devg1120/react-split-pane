e3a89e484e4354754e73b892a659182a3d04e5ea
西草昌市 <devg1120@gmail.com>
2026-02-06 19:30:17 +0900

    up

---
+++ gridsheet/case5/react-core/src/formula/functions/iferror.ts
@@ -0,0 +1,53 @@
+// DO NOT COPY THIS CODE FOR THE OTHER.
+
+import { Table } from '../../lib/table';
+import type { Expression } from '../evaluator';
+import { FormulaError } from '../evaluator';
+import type { FunctionProps } from './__base';
+import { stripTable } from '../../formula/solver';
+
+export class IfErrorFunction {
+  example = 'IFERROR(A1, "Error in cell A1")';
+  helpText = [
+    'Returns the first argument if it is not an error value, otherwise returns the second argument if present, or a blank if the second argument is absent.',
+  ];
+  helpArgs = [
+    {
+      name: 'value',
+      description: 'The value to return if value itself is not an error.',
+    },
+    {
+      name: 'value_if_error',
+      description: 'The value the function returns if value is an error.',
+      optional: true,
+    },
+  ];
+  private args: Expression[];
+  private table: Table;
+
+  constructor({ args, table }: FunctionProps) {
+    this.args = args;
+    this.table = table;
+  }
+
+  protected validate() {
+    if (this.args.length === 1 || this.args.length === 2) {
+      return;
+    }
+    throw new FormulaError(
+      '#N/A',
+      'Number of arguments for IFERROR is incorrect. 1 or 2 argument(s) must be specified.',
+    );
+  }
+
+  public call() {
+    this.validate();
+    const [value, valueIfError] = this.args;
+
+    try {
+      return stripTable({ value: value.evaluate({ table: this.table }) });
+    } catch (e) {
+      return stripTable({ value: valueIfError?.evaluate({ table: this.table }) });
+    }
+  }
+}
---
+++ gridsheet/case5/src/App.tsx
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from "react";
+//import { GridSheet, useHub, type HubProps } from '@gridsheet/react-core';
+import {
+  GridSheet,
+  useHub,
+  makeBorder,
+  type HubProps,
+} from "../react-core/src/index";
+
+
+function colNumToId(colNum) {
+    let columnName = '';
+    while (colNum > 0) {
+        let modulo = (colNum - 1) % 26;
+        columnName = String.fromCharCode(65 + modulo) + columnName;
+        colNum = Math.floor((colNum - modulo) / 26);
+    }
+    return columnName;
+}
+
+const App: React.FC = () => {
+  const [enableDecimalLabeler, setEnableDecimalLabeler] = useState(false);
+
+  const hubProps: HubProps = {
+    labelers: {},
+    onInit: ({ table }) => {
+      console.log(`Table initialized: ${table.sheetName}`);
+    },
+  };
+  const hub = useHub(hubProps);
+
+  useEffect(() => {
+    hubProps.labelers!.decimal = enableDecimalLabeler
+      ? (n: number) => String(n)
+      : null;
+    hub.wire.transmit(hubProps);
+  }, [enableDecimalLabeler]);
+
+  let cells = [];
+
+  for ( let rowNum = 1 ; rowNum < 20 ; rowNum++ ) {
+    for ( let colNum = 1 ; colNum < 20 ; colNum++ ) {
+        const columnName = colNumToId(colNum);
+        const cellName = columnName  +  String(rowNum);
+	//console.log(cellName);
+	cells[cellName] = { value: cellName }
+
+    }
+  }
+  //console.log(cells);
+
+  //style={{ width: 800 }}
+  return (
+    <main>
+      <div className="grid-container">
+        <GridSheet
+          hub={hub}
+/*
+          initialCells={{
+            
+	   // '0': {
+           //       height: 60,  // Header height
+           //       width: 180,   // Header width
+           //     },
+		
+            A1: { value: "Hello" },
+            B1: { value: "React", style: { backgroundColor: "#00bfff" } },
+            A2: { value: 123 },
+            B2: { value: 456 },
+            A3: { value: 789 },
+            C6: { value: "=SUM(A2:B2)" },
+            D7: { value: 789 },
+            E8: { value: 789 },
+            X20: { value: 789 },
+          }}
+*/
+
+         initialCells={ cells }
+
+          options={
+            {
+              //mode: 'dark',
+            }
+          }
+          sheetName="Sheet1"
+          //style={{ width: 800, height: 300 }}
+        />
+
+        <br />
+        <GridSheet
+          hub={hub}
+          initialCells={{
+            default: {
+              width: 150,
+              height: 30,
+              style: { fontSize: "14px" },
+              default: { labeler: "decimal" },
+            },
+
+            C3: { value: "=SUM(Sheet1!A2:B3)" },
+            X20: { value: 789 },
+	             F: {
+                width: 50,
+                renderer: 'checkbox',
+                style: {
+                  backgroundColor: '#f8f9fa',
+                },
+                alignItems: 'center',
+                justifyContent: 'center',
+              },
+            "A7:F7": {
+              style: {
+                ...makeBorder({
+                  bottom: "4px double #000000",
+                }),
+              },
+            },
+            D8: {
+              value: "abc",
+              style: {
+                backgroundColor: "#3498db",
+                color: "white",
+                fontWeight: "bold",
+                textAlign: "center",
+              },
+            },
+            //default: { labeler: 'decimal' },
+          }}
+          //style={{ width: 800, height: 300 }}
+          options={{}}
+          sheetName="Sheet2"
+        />
+      </div>
+      {/* Labeler Control */}
+      <div className="labeler-control">
+        <label>
+          <input
+            type="checkbox"
+            checked={enableDecimalLabeler}
+            onChange={(e) => setEnableDecimalLabeler(e.target.checked)}
+          />
+          Enable Decimal Labeler for Sheet2
+        </label>
+      </div>
+    </main>
+  );
+};
+
+export default App;
